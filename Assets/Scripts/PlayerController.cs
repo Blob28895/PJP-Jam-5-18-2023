6fc9cacaa931d8891f85ea1b84382334e2b4d121
Collin Newman <collinnewman12@gmail.com>
2023-05-18 21:37:55 -0500

    add ResetRun method in PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public int speed = 5;
+    public int availableRuns = 3;
+
+    private int remainingRuns;
     private Vector3 startingPosition;
 
     void Start()
     {
         startingPosition = transform.position;
+        remainingRuns = availableRuns;
     }
 
     void Update()
     {
-        if(Input.GetButtonDown("Reset Run"))
-        {
-            transform.position = startingPosition;
-        }
+        bool resetRun = Input.GetButtonDown("Reset Run"), resetAllRuns = Input.GetButtonDown("Reset All Runs");
+        if(resetRun || resetAllRuns) { ResetRun(resetAllRuns); }
+
+        if(Input.GetButtonDown("Lock In Run")) { remainingRuns--; }
     }
 
     void FixedUpdate()
@@ -32,5 +36,14 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(movement * speed * Time.deltaTime);
+    }
+
+    private void ResetRun(bool allRuns = false)
+    {
+        transform.position = startingPosition;
+        if(allRuns)
+        {
+            remainingRuns = availableRuns;
+        }
     }
 }
