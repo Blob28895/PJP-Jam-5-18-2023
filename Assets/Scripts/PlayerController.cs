6a841a344973bd61522b241875bae7ab9d0b8a21
Collin Newman <collinnewman12@gmail.com>
2023-05-18 20:59:55 -0500

    add player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed = 5;
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+        Debug.Log(movement);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
