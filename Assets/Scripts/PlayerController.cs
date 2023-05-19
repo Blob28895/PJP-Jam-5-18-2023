3ffe767a5a8ea777334d797099d413bca847f51e
Collin Newman <collinnewman12@gmail.com>
2023-05-18 23:06:07 -0500

    add playback functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,16 @@ public class PlayerController : MonoBehaviour
     public int speed = 5;
     public int availableRuns = 3;
 
+    private bool runStarted = false;
     private int remainingRuns;
     private Vector3 startingPosition;
 
+    private Recorder recorder;
+
     void Start()
-    {
+    {  
+        recorder = GetComponent<Recorder>();
+
         startingPosition = transform.position;
         remainingRuns = availableRuns;
     }
@@ -21,12 +26,17 @@ public class PlayerController : MonoBehaviour
         bool resetRun = Input.GetButtonDown("Reset Run"), resetAllRuns = Input.GetButtonDown("Reset All Runs");
         if(resetRun || resetAllRuns) { ResetRun(resetAllRuns); }
 
-        if(Input.GetButtonDown("Lock In Run")) { remainingRuns--; }
+        if(Input.GetButtonDown("Lock In Run")) { LockInRun(); }
     }
 
     void FixedUpdate()
     {
         Move();
+        if(runStarted) 
+        {
+            // also add timer code here
+            recorder.RecordPosition(transform.position, remainingRuns); 
+        }
     }
 
     private void Move()
@@ -35,15 +45,40 @@ public class PlayerController : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(horizontalInput, verticalInput, 0);
+        if(movement != Vector3.zero) { runStarted = true; }
+
         transform.Translate(movement * speed * Time.deltaTime);
     }
 
     private void ResetRun(bool allRuns = false)
     {
+        Debug.Log("Remaining runs: " + remainingRuns);
         transform.position = startingPosition;
-        if(allRuns)
+
+        if(allRuns) { remainingRuns = availableRuns; }
+    }
+
+    private void LockInRun()
+    {
+        if(!runStarted) { return; }
+        if(remainingRuns <= 0) { return; }
+
+        ResetRun();
+        remainingRuns--;
+        
+        if(remainingRuns != 0) { return; }
+
+        for(int i = 1; i < availableRuns + 1; i++)
         {
-            remainingRuns = availableRuns;
+            StartPlayback(i);
         }
+    }
+
+    private void StartPlayback(int run)
+    {
+        GameObject playerClone = Instantiate(gameObject, startingPosition, Quaternion.identity);
+        playerClone.GetComponent<PlayerController>().enabled = false;
+
+        StartCoroutine(recorder.PlaybackRun(run, playerClone, speed));
     }
 }
