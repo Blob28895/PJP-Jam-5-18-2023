7fbfec9e6daaa695d8ca38511f4c829c77a985f5
Collin Newman <collinnewman12@gmail.com>
2023-05-19 19:35:27 -0500

    add door and switch prefabs

---
+++ Assets/Scripts/DoorSwitchController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSwitchController : MonoBehaviour
+{
+    public DoorController door;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
